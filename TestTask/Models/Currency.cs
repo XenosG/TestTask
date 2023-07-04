4cd9f2f87c4a26b934b7da175a14f66551446652
XenosG <x.e.n.o.s.s.g.g@gmail.com>
2023-07-05 00:34:22 +0300

    Added initial models.

---
+++ TestTask/Models/Currency.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Models
+{
+    public class Currency
+    {
+        public string Name { get; }
+
+        public string Code { get; }
+
+        public decimal Price { get; }
+
+        public float PriceChange { get; }
+
+        public decimal Volume { get; }
+
+        public List<Market> Markets { get; }
+
+        public Currency(string name, string code, decimal price, float priceChange, decimal volume, List<Market> markets)
+        {
+            Name = name;
+            Code = code;
+            Price = price;
+            PriceChange = priceChange;
+            Volume = volume;
+            Markets = markets;
+        }
+
+    }
+}
---
+++ TestTask/Models/Market.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Models
+{
+    public class Market
+    {
+        public string Name { get; }
+
+        public decimal Price { get; }
+
+        public Market(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
